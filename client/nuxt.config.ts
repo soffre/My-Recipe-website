4c7a1abb943a2ac268c648b1375c4fe6412ecb51
Amanuel Fentahun <6186fre@gmail.com>
2025-08-22 01:49:42 +0300

    Creating a landing page

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import colors from 'tailwindcss/colors'
+
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
+
+  // tailwindcss: {
+  //   config: {
+  //     theme :{ 
+  //       extend: {
+  //         colors : {primary : colors.green}
+  //       }
+  //     }
+  //   }
+  // }
 })
---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10B981', // Tailwind's green-600
+        secondary: '#FBBF24', // Tailwind's yellow-400
+        accent: '#3B82F6', // Tailwind's blue-500
+        background: '#F3F4F6', // Tailwind's gray-200
+        text: '#111827', // Tailwind's gray-900
+        border: '#D1D5DB', // Tailwind's gray-300
+      }
+    },
+  },
+  plugins: [],
+}
+
